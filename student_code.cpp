b9f7b7d9b7f796fbb4207c4cf21bfcb802b3f05b
leedyun <105830643+leedyun@users.noreply.github.com>
2024-07-29 02:41:01 +0900

    incorrect code

--- student_code.cpp
+++ student_code.cpp
@@ -1,1 +1,17 @@
-// 여기에 작성해주세요
+#include<iostream>
+
+int main()
+{
+	int num;
+	int sum = 1;
+
+	for (int i = 1; i <= 5; i++)
+	{
+		std::cout << i;
+		std::cin >> num;
+		sum += num;
+	}
+	std::cout << "합계: " << sum << std::endl;
+
+	return 0;
+}
